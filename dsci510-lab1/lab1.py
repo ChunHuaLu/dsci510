8c632193f0052d1677e44c995372f9a3d130c42c
ChunHuaLu <chunhua@usc.edu>
2020-09-01 14:16:20 -0700

    dsci510-lab1

---
+++ dsci510-lab1/lab1.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h3 style="color:navy"> DSCI 510 Lab 1  </h3>
+
+# In[1]:
+
+
+print("Welcome to DSCI510!")
+
+
+# <div>
+# <h2> Python for Everybody</h2>
+# <h3 style ="color:navy"> Chapter 1 </h3>
+# </div>
+
+# ### Computer hardware architecture
+# - Central Processing Unit (CPU)
+# - Main Memory
+#     - store information that the CPU needs and is nearly as fast as CPU
+# - Secondary Memory
+#     - slower than main memory, can store memory when there is no power to the computer
+# - Input & Output Devices
+#     - screen, keyboard, mouse...
+
+# ### Interpreter & compiler
+# Interpreter
+# - interpreter is a computer program that directly executes instructions written in a programming or scripting language, without requiring them previously to have been compiled into a machine language program.
+# Compiler
+# - needs to be handed the entire program in a file, and translate high-level source code to machine language and then the compiler puts the resulting machine language into a file for later execution
+
+# <h3 style ="color:navy"> Chapter 2 </h3>
+
+# ### Values & types
+
+# In[2]:
+
+
+print(4)
+print(type("Hello World"))
+print(type(17))
+print(type(3.2))
+print(type("17"))
+print(type("3.2"))
+
+
+# ### Operations and Operands
+
+# In[3]:
+
+
+minute = 59
+print("modulus 59 / 60")
+print(minute/60)
+print("modulus 59 // 60")
+print(minute//60)
+print("remainder")
+print("modulus 59 % 5")
+print(minute%5)
+print("exponential 59*59*59")
+print(minute**3)
+
+
+# ### string operation
+
+# In[4]:
+
+
+first = "100"
+second = "200"
+print(first+second)
+print(first*3)
+
+
+# In[ ]:
+
+
+
+
